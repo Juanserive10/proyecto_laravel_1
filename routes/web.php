0ad63defa3dc0b5926cce666daccce9d482cfcee
Juanserive10 <juriverava@unal.edu.co>
2026-04-18 18:15:36 -0500

    Rutas de Author - Solución conflicto

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,3 +14,10 @@ Route::post("/books",[BookController::class, "store"])->name("books.store");
 Route::get("/books/{id}",[BookController::class, "edit"])->name("books.edit");
 Route::delete("/books/{id}",[BookController::class, "destroy"])->name("books.destroy");
 Route::put("/books/{id}",[BookController::class, "update"])->name("books.update");
+
+//Rutas de Author
+Route::get("/authors",[AuthorController::class, "index"])->name("authors.index");
+Route::post("/authors",[AuthorController::class, "store"])->name("authors.store");
+Route::get("/authors/{id}",[AuthorController::class, "edit"])->name("authors.edit");
+Route::delete("/authors/{id}",[AuthorController::class, "destroy"])->name("authors.destroy");
+Route::put("/authors/{id}",[AuthorController::class, "update"])->name("authors.update");
