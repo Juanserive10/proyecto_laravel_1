eb23f90524920e1463fa0185dbec7cab1c5717b6
Juanserive10 <juriverava@unal.edu.co>
2026-04-18 16:07:22 -0500

    CRUD de Book

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Book;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(){
+        $books = Book::all();
+        return view("books", compact("books"));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request){
+        $book = new Book();
+        $book->name = $request->titulo;
+        $book->price = $request->precio;
+        $book->save();
+        return redirect()->route("books.index");
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($id){
+        $book = Book::find($id);
+        return view("books_edit", compact("book"));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id){
+        $book = Book::find($id);
+        $book->name = $request->titulo;
+        $book->price = $request->precio;
+        $book->save();
+        return redirect()->route("books.index");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id){
+        $book = Book::find($id);
+        $book->delete();
+        return redirect()->route("books.index");
+    }
+}
---
+++ resources/views/books.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <form action="{{route("books.store")}}" method="post">
+            @csrf
+            <label for="a1">Ingrese el Titulo del libro</label>
+            <input type="text" name="titulo" id="a1">
+            <br>
+            <label for="a2">Ingrese el Precio del libro</label>
+            <input type="text" name="precio" id="a2">
+            <button type="submit">Guardar Libro</button>
+
+        </form>
+        <h1>Listado de Libros</h1>
+        <table class = "table">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Nombre</th>
+                    <th>Precio</th>
+                    <th>Opciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($books as $book)
+                    <tr>
+                        <td>{{$book->id}}</td>
+                        <td>{{$book->name}}</td>
+                        <td>{{$book->price}}</td>
+                        <td>
+                            <a href="{{route("books.edit", $book->id)}}" class = "btn btn-warning" >Editar</a>
+
+                            <form action="{{route("books.destroy", $book->id)}}" method="post">
+                                @csrf
+                                @method("delete")
+                                <button type="submit" class = "btn btn-danger">Eliminar</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div> 
+</body>
+</html>
