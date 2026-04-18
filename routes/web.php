9992f905fcef934d065e56101c053ec9769f7acd
Juanserive10 <juriverava@unal.edu.co>
2026-04-18 17:49:23 -0500

    Solución archivo de rutas que no genera conflicto para el merge

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\AuthorController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+
+
+
+
+
 
 Route::get("/authors",[AuthorController::class, "index"])->name("authors.index");
 Route::post("/authors",[AuthorController::class, "store"])->name("authors.store");
